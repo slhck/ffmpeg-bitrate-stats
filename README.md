aa85cb5efcfa5fee5c2c399d6a762ed6e99a9422
Werner Robitza <werner.robitza@gmail.com>
2019-04-19 10:33:22 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,168 @@
+# ffmpeg_quality_metrics
+
+Simple script for calculating bitrate statistics using FFmpeg.
+
+Author: Werner Robitza <werner.robitza@gmail.com>
+
+Contents:
+
+- [Requirements](#requirements)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Output](#output)
+- [License](#license)
+
+------
+
+## Requirements
+
+- Python 3.6
+- FFmpeg:
+    - download a static build from [their website](http://ffmpeg.org/download.html))
+    - put the `ffprobe` executable in your `$PATH`
+- `pip3 install -r requirements.txt`
+
+## Installation
+
+Clone this repo and run `ffmpeg_bitrate_stats.py`.
+
+## Usage
+
+The script outputs a bunch of bitrate statistics, including aggregations for pre-defined windows. These windows can either be time-based or GOP-based (for video streams). When choosing a time-based window, you can specify the size of the chunks in seconds.
+
+Output is to STDOUT so you can redirect that to a file or another script.
+
+See `ffmpeg_bitrate_stats.py -h`:
+
+```
+usage: ffmpeg_bitrate_stats.py [-h] [-n] [-v] [-s {video,audio}]
+                               [-a {time,gop}] [-c CHUNK_SIZE]
+                               [-of {json,csv}]
+                               input
+
+ffmpeg_bitrate_stats v0.0.1
+
+positional arguments:
+  input                 input file
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -n, --dry-run         Do not run command, just show what would be done
+                        (default: False)
+  -v, --verbose         Show verbose output (default: False)
+  -s {video,audio}, --stream-type {video,audio}
+                        Stream type to analyze (default: video)
+  -a {time,gop}, --aggregation {time,gop}
+                        Window for aggregating statistics, either time-based
+                        (per-second) or per GOP (default: time)
+  -c CHUNK_SIZE, --chunk-size CHUNK_SIZE
+                        Custom aggregation window size in seconds (default:
+                        1.0)
+  -of {json,csv}, --output-format {json,csv}
+                        output in which format (default: json)
+```
+
+## Output
+
+The output can be JSON, which includes individual fields for each chunk, or CSV, which repeats each line for each chunk. The CSV adheres to the “tidy” data concept, so it's a little redundant.
+
+Rates are given in kilobit per second, using SI prefixes (i.e., kilo = 1000).
+
+Explanation of the fields:
+
+- `input_file`: Path to the input file
+- `stream_type`: Type of stream used (video, audio)
+- `avg_fps`: Average FPS (number of frames divided by duration)
+- `num_frames`: Number of frames
+- `avg_bitrate`: Average bitrate
+- `avg_bitrate_over_chunks`: Average bitrate calculated over the chunks
+- `max_bitrate`: Maximum bitrate calculated over the chunks
+- `min_bitrate`: Minimum bitrate calculated over the chunks
+- `max_bitrate_factor`: Relation between peak and average
+- `bitrate_per_chunk`: Individual bitrates for each chunk
+- `aggregation`: Type of aggregation used
+- `chunk_size`: Size of the chunk (when aggregation is "time")
+- `duration`: Total duration of the stream (last frame DTS minus first DTS)
+
+JSON example:
+
+```
+➜ ./ffmpeg_bitrate_stats.py -a time -c 30 -of json ~/Downloads/bbb.mp4
+{
+    "input_file": "BigBuckBunny.mp4",
+    "stream_type": "video",
+    "avg_fps": 60.002,
+    "num_frames": 38072,
+    "avg_bitrate": 8002.859,
+    "avg_bitrate_over_chunks": 7849.263,
+    "max_bitrate": 14565.117,
+    "min_bitrate": 3876.533,
+    "max_bitrate_factor": 1.82,
+    "bitrate_per_chunk": [
+        8960.89,
+        8036.678,
+        6099.959,
+        4247.879,
+        7276.979,
+        5738.383,
+        7740.339,
+        7881.705,
+        7572.594,
+        8387.719,
+        9634.343,
+        9939.488,
+        9365.104,
+        5061.071,
+        14565.117,
+        9725.483,
+        4573.873,
+        7765.041,
+        9796.135,
+        12524.024,
+        3876.533,
+        3914.455
+    ],
+    "aggregation": "time",
+    "chunk_size": 30.0,
+    "duration": 634.517
+}
+```
+
+CSV example:
+
+```
+./ffmpeg_bitrate_stats.py -a time -c 30 -of csv ~/Downloads/bbb.mp4
+input_file,chunk_index,stream_type,avg_fps,num_frames,avg_bitrate,avg_bitrate_over_chunks,max_bitrate,min_bitrate,max_bitrate_factor,bitrate_per_chunk,aggregation,chunk_size,duration
+BigBuckBunny.mp4,0,video,60.002,38072,8002.859,7849.263,14565.117,3876.533,1.82,8960.89,time,30.0,634.517
+BigBuckBunny.mp4,1,video,60.002,38072,8002.859,7849.263,14565.117,3876.533,1.82,8036.678,time,30.0,634.517
+BigBuckBunny.mp4,2,video,60.002,38072,8002.859,7849.263,14565.117,3876.533,1.82,6099.959,time,30.0,634.517
+BigBuckBunny.mp4,3,video,60.002,38072,8002.859,7849.263,14565.117,3876.533,1.82,4247.879,time,30.0,634.517
+BigBuckBunny.mp4,4,video,60.002,38072,8002.859,7849.263,14565.117,3876.533,1.82,7276.979,time,30.0,634.517
+BigBuckBunny.mp4,5,video,60.002,38072,8002.859,7849.263,14565.117,3876.533,1.82,5738.383,time,30.0,634.517
+BigBuckBunny.mp4,6,video,60.002,38072,8002.859,7849.263,14565.117,3876.533,1.82,7740.339,time,30.0,634.517
+BigBuckBunny.mp4,7,video,60.002,38072,8002.859,7849.263,14565.117,3876.533,1.82,7881.705,time,30.0,634.517
+BigBuckBunny.mp4,8,video,60.002,38072,8002.859,7849.263,14565.117,3876.533,1.82,7572.594,time,30.0,634.517
+BigBuckBunny.mp4,9,video,60.002,38072,8002.859,7849.263,14565.117,3876.533,1.82,8387.719,time,30.0,634.517
+BigBuckBunny.mp4,10,video,60.002,38072,8002.859,7849.263,14565.117,3876.533,1.82,9634.343,time,30.0,634.517
+BigBuckBunny.mp4,11,video,60.002,38072,8002.859,7849.263,14565.117,3876.533,1.82,9939.488,time,30.0,634.517
+BigBuckBunny.mp4,12,video,60.002,38072,8002.859,7849.263,14565.117,3876.533,1.82,9365.104,time,30.0,634.517
+BigBuckBunny.mp4,13,video,60.002,38072,8002.859,7849.263,14565.117,3876.533,1.82,5061.071,time,30.0,634.517
+BigBuckBunny.mp4,14,video,60.002,38072,8002.859,7849.263,14565.117,3876.533,1.82,14565.117,time,30.0,634.517
+BigBuckBunny.mp4,15,video,60.002,38072,8002.859,7849.263,14565.117,3876.533,1.82,9725.483,time,30.0,634.517
+BigBuckBunny.mp4,16,video,60.002,38072,8002.859,7849.263,14565.117,3876.533,1.82,4573.873,time,30.0,634.517
+BigBuckBunny.mp4,17,video,60.002,38072,8002.859,7849.263,14565.117,3876.533,1.82,7765.041,time,30.0,634.517
+BigBuckBunny.mp4,18,video,60.002,38072,8002.859,7849.263,14565.117,3876.533,1.82,9796.135,time,30.0,634.517
+BigBuckBunny.mp4,19,video,60.002,38072,8002.859,7849.263,14565.117,3876.533,1.82,12524.024,time,30.0,634.517
+BigBuckBunny.mp4,20,video,60.002,38072,8002.859,7849.263,14565.117,3876.533,1.82,3876.533,time,30.0,634.517
+BigBuckBunny.mp4,21,video,60.002,38072,8002.859,7849.263,14565.117,3876.533,1.82,3914.455,time,30.0,634.517
+```
+
+## License
+
+ffmpeg_bitrate_stats, Copyright (c) 2019 Werner Robitza
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ ffmpeg_bitrate_stats.py
@@ -0,0 +1,367 @@
+#!/usr/bin/env python3
+#
+# Calculate bitrate stats from video
+#
+# Output is in kilobit per second unless specified otherwise.
+#
+# Author: Werner Robitza
+# License: MIT
+
+import argparse
+import subprocess
+import math
+import json
+import sys
+import pandas as pd
+import numpy as np
+
+
+def print_stderr(msg):
+    print(msg, file=sys.stderr)
+
+
+def run_command(cmd, dry_run=False, verbose=False):
+    """
+    Run a command directly
+    """
+    if dry_run or verbose:
+        print_stderr("[cmd] " + " ".join(cmd))
+        if dry_run:
+            return None, None
+
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+
+    if process.returncode == 0:
+        return stdout.decode("utf-8"), stderr.decode("utf-8")
+    else:
+        print_stderr("[error] running command: {}".format(" ".join(cmd)))
+        print_stderr(stderr.decode("utf-8"))
+        sys.exit(1)
+
+
+class BitrateStats:
+    def __init__(
+        self,
+        input_file,
+        stream_type="video",
+        aggregation="time",
+        chunk_size=1,
+        dry_run=False,
+        verbose=False,
+    ):
+        self.input_file = input_file
+
+        if stream_type not in ["audio", "video"]:
+            print_stderr("Stream type must be audio/video")
+            sys.exit(1)
+        self.stream_type = stream_type
+
+        if aggregation not in ["time", "gop"]:
+            print_stderr("Wrong aggregation type")
+            sys.exit(1)
+        if aggregation == "gop" and stream_type == "audio":
+            print_stderr("GOP aggregation for audio does not make sense")
+            sys.exit(1)
+        self.aggregation = aggregation
+
+        if chunk_size and chunk_size < 0:
+            print_stderr("Chunk size must be greater than 0")
+            sys.exit(1)
+        self.chunk_size = chunk_size
+
+        self.dry_run = dry_run
+        self.verbose = verbose
+
+        self.duration = 0
+        self.fps = 0
+        self.max_bitrate = 0
+        self.min_bitrate = 0
+        self.moving_avg_bitrate = []
+        self.frames = []
+        self.bitrate_stats = {}
+
+        self.rounding_factor = 3
+
+        self._chunks = []
+
+    def calculate_statistics(self):
+        self._calculate_frame_sizes()
+        self._calculate_duration()
+        self._calculate_fps()
+        self._calculate_max_min_bitrate()
+        self._assemble_bitrate_statistics()
+
+    def _calculate_frame_sizes(self):
+        """
+        Get the frame sizes via ffprobe using the -show_packets option.
+        This includes the NAL headers, of course.
+        """
+        if self.verbose:
+            print_stderr(f"Calculating frame size from {self.input_file}")
+
+        cmd = [
+            "ffprobe",
+            "-loglevel",
+            "error",
+            "-select_streams",
+            self.stream_type[0] + ":0",
+            "-show_packets",
+            "-show_entries",
+            "packet=pts_time,dts_time,duration_time,size,flags",
+            "-of",
+            "json",
+            self.input_file,
+        ]
+
+        stdout, _ = run_command(cmd, self.dry_run)
+        if self.dry_run:
+            print_stderr("Aborting prematurely, dry-run specified")
+            sys.exit(0)
+
+        info = json.loads(stdout)["packets"]
+
+        ret = []
+        idx = 1
+
+        default_duration = next(
+            (x["duration_time"] for x in info if "duration_time" in x.keys()), "NaN"
+        )
+
+        for packet_info in info:
+            frame_type = "I" if packet_info["flags"] == "K_" else "Non-I"
+
+            if "dts_time" in packet_info.keys():
+                dts = float(packet_info["dts_time"])
+            else:
+                dts = "NaN"
+
+            if "duration_time" in packet_info.keys():
+                duration = float(packet_info["duration_time"])
+            else:
+                duration = default_duration
+
+            ret.append(
+                {
+                    "n": idx,
+                    "frame_type": frame_type,
+                    "dts": dts,
+                    "size": int(packet_info["size"]),
+                    "duration": duration,
+                }
+            )
+            idx += 1
+
+        # fix for missing duration in VP9: estimate duration from DTS difference
+        # ret = _fix_durations(ret)
+        self.frames = ret
+        return ret
+
+    def _calculate_duration(self):
+        """
+        Time between first and last DTS.
+        """
+        self.duration = self.frames[-1]["dts"] - self.frames[0]["dts"]
+        return self.duration
+
+    def _calculate_fps(self):
+        """
+        FPS = number of frames divided by duration. A rough estimate.
+        """
+        self.fps = len(self.frames) / self.duration
+        return self.fps
+
+    def _collect_chunks(self):
+        """
+        Collect chunks of a certain aggregation length (in seconds, or GOP).
+        This is cached.
+        """
+        if len(self._chunks):
+            return self._chunks
+
+        if self.verbose:
+            print_stderr("Collecting chunks for bitrate calculation")
+
+        # this is where we will store the stats in buckets
+        aggregation_chunks = []
+        curr_list = []
+
+        if self.aggregation == "gop":
+            # collect group of pictures, each one containing all frames belonging to it
+            for frame in self.frames:
+                if frame["frame_type"] != "I":
+                    curr_list.append(frame)
+                if frame["frame_type"] == "I":
+                    if curr_list:
+                        aggregation_chunks.append(curr_list)
+                    curr_list = [frame]
+            # flush the last one
+            aggregation_chunks.append(curr_list)
+
+        else:
+            # per-time aggregation
+            agg_time = 0
+            for frame in self.frames:
+                if agg_time < self.chunk_size:
+                    curr_list.append(frame)
+                    agg_time += frame["duration"]
+                else:
+                    if curr_list:
+                        aggregation_chunks.append(curr_list)
+                    curr_list = [frame]
+                    agg_time = frame["duration"]
+            aggregation_chunks.append(curr_list)
+
+        # calculate BR per group
+        self._chunks = [
+            BitrateStats._bitrate_for_frame_list(x) for x in aggregation_chunks
+        ]
+
+        return self._chunks
+
+    @staticmethod
+    def _bitrate_for_frame_list(frame_list):
+        """
+        Given a list of frames with size and DTS, get the bitrate,
+        which is done by dividing size through Δ time.
+        """
+        if len(frame_list) < 2:
+            return math.nan
+        size = sum(f["size"] for f in frame_list)
+        times = [f["dts"] for f in frame_list]
+        sum_delta_time = sum(curr - prev for curr, prev in zip(times[1:], times))
+        bitrate = ((size * 8) / 1000) / sum_delta_time
+
+        return bitrate
+
+    def _calculate_max_min_bitrate(self):
+        """
+        Find the min/max from the chunks
+        """
+        self.max_bitrate = max(self._collect_chunks())
+        self.min_bitrate = min(self._collect_chunks())
+        return self.max_bitrate, self.min_bitrate
+
+    def _assemble_bitrate_statistics(self):
+        """
+        Assemble all pre-calculated statistics plus some "easy" ones.
+        """
+
+        self.avg_bitrate = (
+            sum(f["size"] for f in self.frames) * 8 / 1000
+        ) / self.duration
+        self.avg_bitrate_over_chunks = np.mean(self._collect_chunks())
+
+        self.max_bitrate_factor = self.max_bitrate / self.avg_bitrate
+
+        # output data
+        ret = {
+            "input_file": self.input_file,
+            "stream_type": self.stream_type,
+            "avg_fps": round(self.fps, self.rounding_factor),
+            "num_frames": len(self.frames),
+            "avg_bitrate": round(self.avg_bitrate, self.rounding_factor),
+            "avg_bitrate_over_chunks": round(
+                self.avg_bitrate_over_chunks, self.rounding_factor
+            ),
+            "max_bitrate": round(self.max_bitrate, self.rounding_factor),
+            "min_bitrate": round(self.min_bitrate, self.rounding_factor),
+            "max_bitrate_factor": round(self.max_bitrate_factor, self.rounding_factor),
+            "bitrate_per_chunk": [
+                round(b, self.rounding_factor) for b in self._collect_chunks()
+            ],
+            "aggregation": self.aggregation,
+            "chunk_size": self.chunk_size,
+            "duration": round(self.duration, self.rounding_factor),
+        }
+
+        self.bitrate_stats = ret
+        return self.bitrate_stats
+
+    def print_statistics(self, output_format):
+        if output_format == "csv":
+            self._print_csv()
+        elif output_format == "json":
+            self._print_json()
+
+    def _print_csv(self):
+        df = pd.DataFrame(self.bitrate_stats)
+        df.reset_index(level=0, inplace=True)
+        df.rename(index=str, columns={"index": "chunk_index"}, inplace=True)
+        cols = df.columns.tolist()
+        cols.insert(0, cols.pop(cols.index("input_file")))
+        df = df.reindex(columns=cols)
+        print(df.to_csv(index=False))
+
+    def _print_json(self):
+        print(json.dumps(self.bitrate_stats, indent=4))
+
+
+def main():
+    version = "0.0.1"
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        description="ffmpeg_bitrate_stats v" + version,
+    )
+    parser.add_argument("input", help="input file")
+
+    parser.add_argument(
+        "-n",
+        "--dry-run",
+        action="store_true",
+        help="Do not run command, just show what would be done",
+    )
+
+    parser.add_argument(
+        "-v", "--verbose", action="store_true", help="Show verbose output"
+    )
+
+    parser.add_argument(
+        "-s",
+        "--stream-type",
+        default="video",
+        choices=["video", "audio"],
+        help="Stream type to analyze",
+    )
+
+    parser.add_argument(
+        "-a",
+        "--aggregation",
+        default="time",
+        choices=["time", "gop"],
+        help="Window for aggregating statistics, either time-based (per-second) or per GOP",
+    )
+
+    parser.add_argument(
+        "-c",
+        "--chunk-size",
+        type=float,
+        default=1.0,
+        help="Custom aggregation window size in seconds",
+    )
+
+    parser.add_argument(
+        "-of",
+        "--output-format",
+        type=str,
+        default="json",
+        choices=["json", "csv"],
+        help="output in which format",
+    )
+
+    cli_args = parser.parse_args()
+
+    br = BitrateStats(
+        cli_args.input,
+        cli_args.stream_type,
+        cli_args.aggregation,
+        cli_args.chunk_size,
+        cli_args.dry_run,
+        cli_args.verbose,
+    )
+    br.calculate_statistics()
+    br.print_statistics(cli_args.output_format)
+
+
+if __name__ == "__main__":
+    main()
