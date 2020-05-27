0882d6db47d3df7c1384f73cd7e3f116860c5bcc
Grzegorz Klimaszewski <grzegorz.klimaszewski@yahoo.com>
2020-05-27 11:30:07 +0200

    Changing version number

--- setup.py
+++ setup.py
@@ -36,7 +36,8 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     install_requires=[
         'numpy',
