2dcf1f103e9315530b9ee4f56aa14252a215eb48
Werner Robitza <werner.robitza@gmail.com>
2025-06-17 13:46:43 +0200

    set python_requires to >=3.9

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
     ],
-    python_requires=">=3.8",
+    python_requires=">=3.9",
     packages=["ffmpeg_bitrate_stats"],
     install_requires=["numpy", "pandas", "plotille"],
     include_package_data=True,
